e5e4298887f17362175f5c8fe56965bad988725b
Daniel Roizen <droizen@gmail.com>
2019-10-11 01:20:58 +0100

    changed file

--- Week 3/serverTest.py
+++ Week 3/serverTest.py
@@ -1,7 +1,7 @@
 import socket
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-serv.bind(('192.168.43.141', 8080))
+serv.bind(('0.0.0.0', 8080))
 serv.listen(5)
 while True:
     conn, addr = serv.accept()
@@ -9,7 +9,7 @@ while True:
     while True:
         data = conn.recv(4096)
         if not data: break
-        from_client += data
+        from_client += data.encode()
         print(from_client)
         conn.send("I am SERVER\n")
     conn.close()
